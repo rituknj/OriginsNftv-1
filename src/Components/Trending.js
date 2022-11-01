933eee12cc95b1d5bf1cf38be48ffb60bf1ea09e
kesaviwebsolutions <rituknj@gmail.com>
2022-11-01 16:20:01 +0530

    update-color

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -22,36 +22,57 @@ function UncontrolledExample(props) {
       <div className="container-fluid py-5 px-5">
         <div className="row">
           <Tabs>
-            <TabList>
-              <Tab>Trending</Tab>
-              <Tab>Top</Tab>
-              <span className="h24">View All</span>
+            <TabList style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}>
+              <Tab style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}>Trending</Tab>
+              <Tab style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}>Top</Tab>
+              <span className="h24" style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}>View All</span>
               <button
                 className="h25"
                 type="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
+                style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}
               >
-                24 h <AiOutlineCaretDown />
+                24 h <AiOutlineCaretDown style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}/>
               </button>
-              <ul className="dropdown-menu">
+              <ul className="dropdown-menu" >
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <a className="dropdown-item" href="/" style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}>
                     24 h
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <a className="dropdown-item" href="/" style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}>
                     7 d
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <a className="dropdown-item" href="/" style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}>
                     30 d
                   </a>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <a className="dropdown-item" href="/" style={{
+                  color: props.mode === "light" ? "black " : "white ",
+                }}>
                     All
                   </a>
                 </li>
